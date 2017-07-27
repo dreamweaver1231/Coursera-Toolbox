db991b62b862de489bb3a37e1ca6de4fa2959d5a
Pratap Singh <dreamweaver1231@gmail.com>
2017-07-27 13:31:53 +0530

    added comments and project file for sublime

--- Week2/01_introduction_starter_files/fibonacci_huge/fibonacci_huge.py
+++ Week2/01_introduction_starter_files/fibonacci_huge/fibonacci_huge.py
@@ -1,5 +1,13 @@
 # Uses python3
 
+"""Find the last digit of a sum of the  rst 𝑛 Fibonacci numbers.
+
+Instead of computing this sum in a loop, try to come up with a formula
+for 𝐹0 + 𝐹1 + 𝐹2 + ··· + 𝐹𝑛. For this, play with small values of 𝑛.
+Then, use a solution for the previous problem.
+"""
+
+
 def get_sequence(mod):
     previous = 0
     current = 1
@@ -13,11 +21,13 @@ def get_sequence(mod):
         if(arr[width - 2] == 0 and arr[width - 1] == 1):
             return arr[: -2]
 
+
 def get_fibonacci_huge_naive(num, mod):
     if num <= 1:
         return num
     seq = get_sequence(mod)
     return seq[num % len(seq)]
+
 
 if __name__ == '__main__':
     data = input()
